dc159903b71746c01ad038136df33146fcd65644
skmathew <skm20011@gmail.com>
2015-04-22 23:15:36 -0500

    function to filter out the bad words

--- ProxyServer.c
+++ ProxyServer.c
@@ -27,6 +27,7 @@
 int threadCount = BACKLOG;
 void *client_handler(void *sock_desc);
 int blocked_websites(char *name);
+char *str_replace ( const char *string, const char *substr, const char *replacement );
 
 
 /*----------------------------------------------
@@ -160,7 +161,7 @@ void *client_handler(void *sock_desc) {
 				else
                 {
                  // Get page from cache
-                 fp=fopen(pch, "r");
+                    fp=fopen(pch, "r");
                     
                     if (fp != NULL) {
                         /* Go to the end of the file. */
@@ -185,12 +186,12 @@ void *client_handler(void *sock_desc) {
                             }
                         }
                         fclose(fp);
-                        printf("%s",source);
-                        send(sock, source, count, 0);
-                        free(source);
+                        //printf("%s",source);
+                        source=str_replace(source,"Your","*****");
+                        
+                        send(sock, source, count+1, 0);
+                        //free(source);
                     }
-
-
                 }
 			//}
 		}
@@ -334,6 +335,7 @@ int GET_page( char *host )
 	
 	while((tmpres = recv(GET_SOCK, buf, BUFSIZ, 0)) > 0)
 	{
+        
 		if(htmlstart == 0) {
 			htmlcontent = strstr(buf, "\r\n\r\n");
 			if(htmlcontent != NULL){
@@ -365,10 +367,36 @@ int GET_page( char *host )
 }
 
 
-/*----------------------------------------------
- *	LOADS THE CACHE TO BUFFER
- *	Return 0 on fail, 1 on success
- *----------------------------------------------*/
+
+
+char *str_replace ( const char *string, const char *substr, const char *replacement ){
+    char *tok = NULL;
+    char *newstr = NULL;
+    char *oldstr = NULL;
+    char *head = NULL;
+    
+    /* if either substr or replacement is NULL, duplicate string a let caller handle it */
+    if ( substr == NULL || replacement == NULL ) return strdup (string);
+    newstr = strdup (string);
+    head = newstr;
+    while ( (tok = strstr ( head, substr ))){
+        oldstr = newstr;
+        newstr = malloc ( strlen ( oldstr ) - strlen ( substr ) + strlen ( replacement ) + 1 );
+        /*failed to alloc mem, free old string and return NULL */
+        if ( newstr == NULL ){
+            free (oldstr);
+            return NULL;
+        }
+        memcpy ( newstr, oldstr, tok - oldstr );
+        memcpy ( newstr + (tok - oldstr), replacement, strlen ( replacement ) );
+        memcpy ( newstr + (tok - oldstr) + strlen( replacement ), tok + strlen ( substr ), strlen ( oldstr ) - strlen ( substr ) - ( tok - oldstr ) );
+        memset ( newstr + strlen ( oldstr ) - strlen ( substr ) + strlen ( replacement ) , 0, 1 );
+        /* move back head right after the last replacement */
+        head = newstr + (tok - oldstr) + strlen( replacement );
+        free (oldstr);
+    }
+    return newstr;
+}
 
 
 
